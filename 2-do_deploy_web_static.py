490635d520527e7b66b9e878657acb3ea244d73d
AnasMouak <anasmouak1111@gmail.com>
2024-05-03 13:24:19 -0700

    Update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,65 +1,25 @@
 #!/usr/bin/python3
-"""
-Fabric script to distribute an archive to web servers.
-"""
-
+'''Fabric script that distributes an archive to web servers, using the function do_deploy'''
 from fabric.api import put, run, env
-from fabric.context_managers import hide
-from os.path import exists
-
+import os
 env.hosts = ['100.26.218.180', '35.168.8.95']
 
 
 def do_deploy(archive_path):
-    """
-    Distribute an archive to web servers.
-
-    Args:
-        archive_path (str): The path of the archive to distribute.
-
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
-    """
-    if not exists(archive_path):
+    """distributes an archive to the web servers"""
+    if not os.path.exists(archive_path):
         return False
-
     try:
-        # Upload the archive to the /tmp/ directory of the web server
-        with hide('running'):
-            put(archive_path, '/tmp/')
-
-        # Get the filename without extension
-        filename = archive_path.split('/')[-1].split('.')[0]
-
-        # Uncompress the archive to the releases folder
-        with hide('running'):
-            run('mkdir -p /data/web_static/releases/{}/'.format(filename))
-            run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'.format(
-                filename, filename))
-
-        # Remove the archive from the web server
-        with hide('running'):
-            run('rm /tmp/{}.tgz'.format(filename))
-
-        # Move the contents of the web_static folder one level up
-        with hide('running'):
-            run('mv /data/web_static/releases/{}/web_static/* '
-                '/data/web_static/releases/{}/'.format(filename, filename))
-            run('rm -rf /data/web_static/releases/{}/web_static'.format(
-                filename))
-
-        # Remove the current symlink
-        with hide('running'):
-            run('rm -rf /data/web_static/current')
-
-        # Create a new symlink pointing to the new release
-        with hide('running'):
-            run('ln -s /data/web_static/releases/{}/ '
-                '/data/web_static/current'.format(filename))
-
+        put(archive_path, '/tmp/')
+        no_ext_arch_path = os.path.basename(archive_path).split(".")[0]
+        run(f'mkdir -p /data/web_static/releases/{no_ext_arch_path}/')
+        run(f'tar -xzf /tmp/{no_ext_arch_path}.tgz -C /data/web_static/releases/{no_ext_arch_path}/')
+        run(f'rm /tmp/{no_ext_arch_path}.tgz')
+        run(f'mv /data/web_static/releases/{no_ext_arch_path}/web_static/* /data/web_static/releases/{no_ext_arch_path}/')
+        run(f'rm -rf /data/web_static/releases/{no_ext_arch_path}/web_static')
+        run('rm -rf /data/web_static/current')
+        run(f'ln -s /data/web_static/releases/{no_ext_arch_path}/ /data/web_static/current')
         print("New version deployed!")
         return True
-
     except Exception:
         return False
