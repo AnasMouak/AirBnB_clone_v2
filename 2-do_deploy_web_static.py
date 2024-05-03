76d784fb9f2fc2511f740fb2901127c044ce5993
AnasMouak <anasmouak1111@gmail.com>
2024-05-03 09:05:25 -0700

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,26 +1,33 @@
 #!/usr/bin/python3
-'''Fabric script that distributes an archive to web servers, using the function do_deploy'''
-
+'''Fabric script that distributes an archive to web servers, using the function do_deploy'''                            
 from fabric.api import put, run, env
+from datetime import datetime
+from fabric.api import local
 import os
 env.hosts = ['100.26.218.180', '35.168.8.95']
 
-
+def do_pack():
+    """generates a tgz archive"""
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
+    if not os.path.exists("versions"):
+        local("mkdir versions")
+    local(f"tar -czvf versions/web_static_{time}.tgz web_static")
+    if os.path.exists(f"versions/web_static_{time}.tgz"):
+        return f"versions/web_static_{time}.tgz"
+    else:
+        return None
+    
 def do_deploy(archive_path):
     """distributes an archive to the web servers"""
-    if os.path.exists(archive_path) is False:
+    no_ext_arch_path = archive_path.split("/")[-1].split(".")[0]
+    archive_path = do_pack()
+    if not os.path.exists(archive_path):
         return False
-    try:
-        fil =  archive_path.split("/")[-1]
-        no_ext_arch_path = fil.split(".")[0]
-        put(archive_path, '/tmp/')
-        run(f'mkdir -p /data/web_static/releases/{no_ext_arch_path}/')
-        run(f'tar -xzf /tmp/{fil} -C /data/web_static/releases/{no_ext_arch_path}/')
-        run(f'rm /tmp/{fil}')
-        run(f'mv /data/web_static/releases/{no_ext_arch_path}/web_static/* /data/web_static/releases/{no_ext_arch_path}/')
-        run(f'rm -rf /data/web_static/releases/{no_ext_arch_path}/web_static')
-        run('rm -rf /data/web_static/current')
-        run(f'ln -s /data/web_static/releases/{no_ext_arch_path}/ /data/web_static/current')
-        return True
-    except:
-        return False
+    put(archive_path, '/tmp/')
+    run(f'mkdir -p /data/web_static/releases/{no_ext_arch_path}')
+    run(f'tar -xzf /tmp/{archive_path.split("/")[-1]} -C/data/web_static/releases/{no_ext_arch_path}')
+    run(f'rm /tmp/{archive_path.split("/")[-1]}')
+    run(f'mv /data/web_static/releases/{no_ext_arch_path}/web_static/*/data/web_static/releases/{no_ext_arch_path}')
+    run(f'rm -rf /data/web_static/releases/{no_ext_arch_path}/web_static')
+    run('rm -rf /data/web_static/current')
+    run(f'ln -s /data/web_static/releases/{no_ext_arch_path} /data/web_static/current')
