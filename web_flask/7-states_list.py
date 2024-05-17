56f1d0ad34a24bedad4bc1db7f3e71106b384dc8
AnasMouak <anasmouak1111@gmail.com>
2024-05-17 16:00:03 -0700

    Update List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -16,7 +16,7 @@ def teardown_db(exception):
 def states_list():
     """return states list"""
     states = storage.all("State").values()
-    sorted_states = sorted(states, key=lambda x: x.name)
+    sorted_states = sorted(list(states), key=lambda x: x.name)
     return render_template('7-states_list.html', states=sorted_states)
 
 
