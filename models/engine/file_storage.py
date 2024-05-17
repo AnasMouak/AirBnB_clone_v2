3d74cc965de046c6592083255284d9334b717f7b
AnasMouak <anasmouak1111@gmail.com>
2024-05-17 15:28:29 -0700

    Improve engines

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -79,3 +79,7 @@ class FileStorage:
             key = f"{obj.__class__.__name__}.{obj.id}"
             if key in FileStorage.__objects:
                 del FileStorage.__objects[key]
+
+    def close(self):
+        """deserializing the JSON file to objects"""
+        self.reload()
